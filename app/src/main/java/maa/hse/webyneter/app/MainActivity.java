bc61a39462376805bd67752a67d46d34bd9d7c12
WEBYNETER-PC\webyn <webyneter@gmail.com>
2016-09-17 14:03:34 +0300

    - code cleaned up

--- app/src/main/java/maa/hse/webyneter/app/MainActivity.java
+++ app/src/main/java/maa/hse/webyneter/app/MainActivity.java
@@ -13,10 +13,21 @@ import maa.hse.webyneter.app.task6.Task6Activity;
 
 // TODO: 9/15/2016 preemptively request all necessary permissions
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnTask1;
+    private Button btnTask2;
+    private Button btnTask3;
+    private Button btnTask4;
+    private Button btnTask5;
+    private Button btnTask6;
+    private Button btnTask7;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initializeUiControls();
 
 //        ActionBar bar = getSupportActionBar();
 //        if (bar != null) {
@@ -26,18 +37,27 @@ public class MainActivity extends AppCompatActivity {
 
 //        android.app.ActionBar t = getActionBar();
 
-        redirectToActivityOnButtonClick(R.id.btnTask1, Task1Activity.class);
-        redirectToActivityOnButtonClick(R.id.btnTask2, Task2Activity.class);
-//        redirectToActivityOnButtonClick(R.id.btnTask3, Task3Activity.class);
-        redirectToActivityOnButtonClick(R.id.btnTask4, Task4Activity.class);
-//        redirectToActivityOnButtonClick(R.id.btnTask5, Task5Activity.class);
-        redirectToActivityOnButtonClick(R.id.btnTask6, Task6Activity.class);
-//        redirectToActivityOnButtonClick(R.id.btnTask7, Task7Activity.class);
+        redirectToActivityOnButtonClick(btnTask1, Task1Activity.class);
+        redirectToActivityOnButtonClick(btnTask2, Task2Activity.class);
+//        redirectToActivityOnButtonClick(btnTask3, Task3Activity.class);
+        redirectToActivityOnButtonClick(btnTask4, Task4Activity.class);
+//        redirectToActivityOnButtonClick(btnTask5, Task5Activity.class);
+        redirectToActivityOnButtonClick(btnTask6, Task6Activity.class);
+//        redirectToActivityOnButtonClick(btnTask7, Task7Activity.class);
     }
 
-    private void redirectToActivityOnButtonClick(final int buttonId,
-                                                 final Class<?> redirectToActivityClass) {
-        final Button btn = (Button) findViewById(buttonId);
+    private void initializeUiControls() {
+        btnTask1 = (Button) findViewById(R.id.btnTask1);
+        btnTask2 = (Button) findViewById(R.id.btnTask2);
+        btnTask3 = (Button) findViewById(R.id.btnTask3);
+        btnTask4 = (Button) findViewById(R.id.btnTask4);
+        btnTask5 = (Button) findViewById(R.id.btnTask5);
+        btnTask6 = (Button) findViewById(R.id.btnTask6);
+        btnTask7 = (Button) findViewById(R.id.btnTask7);
+    }
+
+    private void redirectToActivityOnButtonClick(final Button btn, final Class<?> redirectToActivityClass) {
+        btn.setEnabled(true);
         btn.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, redirectToActivityClass);
