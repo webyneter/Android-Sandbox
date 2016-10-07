8988efc174b6ef59e7e68f8f7989c3788118ba7b
WEBYNETER-PC\webyn <webyneter@gmail.com>
2016-10-07 15:53:22 +0300

    - task1 enhanced

--- app/src/main/java/maa/hse/webyneter/app/task1/Task1Activity.java
+++ app/src/main/java/maa/hse/webyneter/app/task1/Task1Activity.java
@@ -5,6 +5,8 @@ import android.support.v4.view.GestureDetectorCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
+import android.view.View;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import maa.hse.webyneter.app.R;
@@ -12,6 +14,18 @@ import maa.hse.webyneter.app.R;
 public class Task1Activity extends AppCompatActivity implements GestureDetector.OnGestureListener,
         GestureDetector.OnDoubleTapListener {
     private TextView tvGesture;
+
+    private RelativeLayout rlXandY;
+    private TextView tvX;
+    private TextView tvY;
+
+    private RelativeLayout rlFlingVelocities;
+    private TextView tvFlingVelocityX;
+    private TextView tvFlingVelocityY;
+
+    private RelativeLayout rlScrollDistances;
+    private TextView tvScrollDistanceX;
+    private TextView tvScrollDistanceY;
 
     private GestureDetectorCompat gestureDetector;
 
@@ -23,11 +37,32 @@ public class Task1Activity extends AppCompatActivity implements GestureDetector.
         initUiVariables();
 
         gestureDetector = new GestureDetectorCompat(this, this);
+
+        initUiHandlers();
+
+        rlXandY.setVisibility(View.GONE);
+        rlFlingVelocities.setVisibility(View.GONE);
+        rlScrollDistances.setVisibility(View.GONE);
+    }
+
+    private void initUiHandlers() {
         gestureDetector.setOnDoubleTapListener(this);
     }
 
     private void initUiVariables() {
         tvGesture = (TextView) findViewById(R.id.task1_tvGesture);
+
+        rlXandY = (RelativeLayout) findViewById(R.id.task1_rlXandY);
+        tvX = (TextView) findViewById(R.id.task1_tvX);
+        tvY = (TextView) findViewById(R.id.task1_tvY);
+
+        rlFlingVelocities = (RelativeLayout) findViewById(R.id.task1_rlFlingVelocities);
+        tvFlingVelocityX = (TextView) findViewById(R.id.task1_tvFlingVelocityX);
+        tvFlingVelocityY = (TextView) findViewById(R.id.task1_tvFlingVelocityY);
+
+        rlScrollDistances = (RelativeLayout) findViewById(R.id.task1_rlScrollDistances);
+        tvScrollDistanceX = (TextView) findViewById(R.id.task1_tvScrollDistanceX);
+        tvScrollDistanceY = (TextView) findViewById(R.id.task1_tvScrollDistanceY);
     }
 
     @Override
@@ -43,44 +78,41 @@ public class Task1Activity extends AppCompatActivity implements GestureDetector.
 
     @Override
     public boolean onDown(MotionEvent event) {
-        tvGesture.setText(GestureKind.onDown.getValue());
+        handleGesture(GestureKind.onDown, event);
         return true;
     }
 
     @Override
-    public boolean onFling(MotionEvent event1, MotionEvent event2,
-                           float velocityX, float velocityY) {
-        tvGesture.setText(GestureKind.onFling.getValue());
+    public boolean onFling(MotionEvent event1, MotionEvent event2, float velocityX, float velocityY) {
+        handleFling(event1, event2, velocityX, velocityY);
         return true;
     }
 
     @Override
     public void onLongPress(MotionEvent event) {
-        tvGesture.setText(GestureKind.onLongPress.getValue());
-
+        handleGesture(GestureKind.onLongPress, event);
     }
 
     @Override
-    public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX,
-                            float distanceY) {
-        tvGesture.setText(GestureKind.onScroll.getValue());
+    public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+        handleScroll(e1, e2, distanceX, distanceY);
         return true;
     }
 
     @Override
     public void onShowPress(MotionEvent event) {
-        tvGesture.setText(GestureKind.onShowPress.getValue());
+        handleGesture(GestureKind.onShowPress, event);
     }
 
     @Override
     public boolean onSingleTapUp(MotionEvent event) {
-        tvGesture.setText(GestureKind.onSingleTapUp.getValue());
+        handleGesture(GestureKind.onSingleTap, event);
         return true;
     }
 
     @Override
     public boolean onDoubleTap(MotionEvent event) {
-        tvGesture.setText(GestureKind.onDoubleTap.getValue());
+        handleGesture(GestureKind.onDoubleTap, event);
         return true;
     }
 
@@ -91,8 +123,36 @@ public class Task1Activity extends AppCompatActivity implements GestureDetector.
 
     @Override
     public boolean onSingleTapConfirmed(MotionEvent event) {
-        tvGesture.setText(GestureKind.onSingleTap.getValue());
+        handleGesture(GestureKind.onSingleTap, event);
         return true;
+    }
+
+    private void handleGesture(GestureKind gestureKind, MotionEvent event) {
+        rlFlingVelocities.setVisibility(View.GONE);
+        rlScrollDistances.setVisibility(View.GONE);
+
+        tvGesture.setText(gestureKind.getValue());
+        tvX.setText(Float.toString(event.getX()));
+        tvY.setText(Float.toString(event.getY()));
+        rlXandY.setVisibility(View.VISIBLE);
+    }
+
+    private void handleFling(MotionEvent event1, MotionEvent event2, float velocityX, float velocityY) {
+        rlXandY.setVisibility(View.GONE);
+
+        tvFlingVelocityX.setText(Float.toString(velocityX));
+        tvFlingVelocityY.setText(Float.toString(velocityY));
+        rlScrollDistances.setVisibility(View.GONE);
+        rlFlingVelocities.setVisibility(View.VISIBLE);
+    }
+
+    private void handleScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+        rlXandY.setVisibility(View.GONE);
+
+        tvScrollDistanceX.setText(Float.toString(distanceX));
+        tvScrollDistanceY.setText(Float.toString(distanceY));
+        rlFlingVelocities.setVisibility(View.GONE);
+        rlScrollDistances.setVisibility(View.VISIBLE);
     }
 
     /**
